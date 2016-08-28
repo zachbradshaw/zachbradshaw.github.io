455d8017ccff45b0ec38935ad960fc122b48369c
Zach Bradshaw <zachary.bradshaw@bronto.com>
2016-08-28 16:25:13 -0400

    React setup, Name component

---
+++ src/app/Portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'React';
+import Name from './components/Name';
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <Name />
+        );
+    }
+}
+
+export default Portfolio;
---
+++ src/app/components/Name.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'React';
+
+class Name extends Component {
+    render() {
+        return (
+            <div className='name-container'>
+                <h1 className='info__name'>Zach Bradshaw</h1>
+                <h3 className='info__title'>Front End Developer</h3>
+            </div>
+        );
+    }
+}
+
+export default Name;
