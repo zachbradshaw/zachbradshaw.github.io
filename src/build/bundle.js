2cb8e359b1df701743fd021953d1496e0daa7483
zachbradshaw <bradshaw.zachary@gmail.com>
2016-12-24 14:51:42 -0500

    File type check

--- src/build/bundle.js
+++ src/build/bundle.js
@@ -25902,40 +25902,40 @@
 	var projects = [{
 	    name: 'Hackstarter',
 	    desc: 'My final project for The Iron Yard. I worked on a team of five developers to\n    finish this project in just two weeks. Hackstarter is a crowdfunding platform for students\n    interested in going to code school. I implemented many features on the site including the\n    interactive code school map using the Google Maps API and the tuition calculator which I\n    built using AngularJS.',
-	    imgSrc: './app/lib/images/hackstarter.png',
+	    imgSrc: './app/lib/images/hackstarter.PNG',
 	    imgAlt: 'Hackstarter Homepage'
 	}, {
 	    name: 'Memory!',
 	    desc: 'A memory game built in jQuery. A user must match all tiles to win the game.\n    When they guess incorrectly their lives decrement. The game is over when a user either\n    matches all tiles or their lives run out. Working out the matching logic for the game\n    tiles helped to strengthen my Javascript and jQuery skills. The styling and animations\n    were done in SCSS.',
-	    imgSrc: './app/lib/images/memory.png',
+	    imgSrc: './app/lib/images/memory.PNG',
 	    imgAlt: 'Memory Game',
 	    siteLink: 'http://zachbradshaw.github.io/memory',
 	    repoLink: 'https://www.github.com/zachbradshaw/memory'
 	}, {
 	    name: '(resource)ful',
 	    desc: '(resource)ful is a Reddit clone meant for developers who want to share resources with\n    other developers. This was a weekend homework project for The Iron Yard and was my\n    first experience deploying a site to Heroku. This project also introduced me to the\n    wonderful world of AngularJS, and we used Github OAuth to handle user login.',
-	    imgSrc: './app/lib/images/resourceful.png',
+	    imgSrc: './app/lib/images/resourceful.PNG',
 	    imgAlt: '(resource)ful',
 	    siteLink: 'http://tiy-resourceful.herokuapp.com/',
 	    repoLink: 'https://github.com/zachbradshaw/reshare'
 	}, {
 	    name: 'Appointment Tracker',
 	    desc: 'A mobile web-app that tracks appointments and events. Uses the OpenWeatherMap API to\n    give the current temperature and weather for the location of the appointment. This was\n    my first project that used local storage to save information, so a users appointments\n    are saved each time they visit the site.',
-	    imgSrc: './app/lib/images/todoapp.png',
+	    imgSrc: './app/lib/images/todoapp.PNG',
 	    imgAlt: 'Appointment Tracker',
 	    siteLink: 'http://zachbradshaw.github.io/sunnyappointments/',
 	    repoLink: 'https://github.com/zachbradshaw/sunnyappointments'
 	}, {
 	    name: 'Sign Up Flow',
 	    desc: 'This is a mock up of a user management system for an admin page. This was my first\n    project using jQuery and doing true DOM manipulation. An admin can add users and\n    their emails and then delete them if necessary.',
-	    imgSrc: './app/lib/images/signup.png',
+	    imgSrc: './app/lib/images/signup.PNG',
 	    imgAlt: 'Sign up flow',
 	    siteLink: 'http://zachbradshaw.github.io/user-mgmt/',
 	    repoLink: 'https://github.com/zachbradshaw/user-mgmt'
 	}, {
 	    name: 'Hangman',
 	    desc: 'My first Javascript project, a hangman letter game. This was another weekend homework\n    assignment for The Iron Yard, and it was a great first exercise in some basic\n    programming principles.',
-	    imgSrc: './app/lib/images/hangman.png',
+	    imgSrc: './app/lib/images/hangman.PNG',
 	    imgAlt: 'Hangman game',
 	    siteLink: 'http://zachbradshaw.github.io/week2/hangman.html',
 	    repoLink: 'https://github.com/zachbradshaw/week2'
